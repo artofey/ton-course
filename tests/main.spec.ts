7c9a5b657cc902965f2c379d3e81d7c556f9bd83
Artem Yarovenko <a.yarovenko@rubetek.com>
2024-07-05 12:03:54 +0700

    4.2 done

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,22 +1,29 @@
-import { Cell, toNano } from "ton-core"
+import { Address, Cell, toNano } from "ton-core"
 import { hex } from "../build/main.compiled.json"
 import { Blockchain, printTransactionFees } from "@ton-community/sandbox"
 import { MainContract } from "../wrappers/MainContract"
 import "@ton-community/test-utils"
 
 describe("main.fc contract tests", () => {
-    it("should get the proper most recent sender address",  async () => {
-        const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0]
-    
+    it("should successfully increase counter in contract and get the proper most recent sender address",  async () => {
         const blockchain = await Blockchain.create()
+        const initAddress = await blockchain.treasury("initAddress")
+        const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0]
 
         const myContract = blockchain.openContract(
-            await MainContract.createFromConfig({},codeCell) 
+            MainContract.createFromConfig({
+                number: 0,
+                address: initAddress.address,
+            },codeCell) 
         )
 
         const senderWallet = await blockchain.treasury("sender")
         
-        const sentMessageResult = await myContract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"))
+        const sentMessageResult = await myContract.sendIncrement(
+            senderWallet.getSender(),
+            toNano("0.05"),
+            1
+        )
         expect(sentMessageResult.transactions).toHaveTransaction({
             from: senderWallet.address,
             to: myContract.address,
@@ -24,8 +31,8 @@ describe("main.fc contract tests", () => {
         })
         
         const data = await myContract.getData()
+        
         expect(data.recent_sender.toString()).toBe(senderWallet.address.toString())
-
-        printTransactionFees(sentMessageResult.transactions)
+        expect(data.number).toEqual(1)
     })
 })
